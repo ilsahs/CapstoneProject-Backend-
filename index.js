26bcc7152efcd0b8762170cb8083504aa3ebaf92
shifa004 <135617151+shifa004@users.noreply.github.com>
2024-03-01 20:08:42 +0300

    updated events and chatbot prompts

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const cron = require('node-cron');
 const fs = require('fs');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
+
 //Models
 const UserModel = require('./models/Users')
 const EventModel = require('./models/Events')
@@ -81,10 +82,10 @@ const readFile = promisify(fs.readFile);
 
 //Schedule web scraping for every hour: 0 * * * *
 //every 5 minutes: */5 * * * *
-cron.schedule('0 * * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
 // const scrape = async () => {
     console.log('Running Python script...');
-        // Execute the Python script
+    // Execute the Python script
     try {
         const { stdout, stderr } = await exec(command);
         if (stderr) {
@@ -96,7 +97,7 @@ cron.schedule('0 * * * *', async () => {
         const data = await readFile(jsonFilePath, 'utf8');
         const scrapedEvents = JSON.parse(data);
         // Assuming updateEvents is an async function
-        await updateEvents(scrapedEvents);
+        updateEvents(scrapedEvents);
     } catch (error) {
         console.error(`Error: ${error}`);
     }
@@ -107,6 +108,7 @@ cron.schedule('0 * * * *', async () => {
 });
 
 const updateEvents = (scrapedEvents) => {
+    console.log(`Total Events: ${scrapedEvents.length}`)
     scrapedEvents.forEach (async e => {
     await EventModel.findOneAndUpdate(
         //check if the event exists based on the name
@@ -124,7 +126,7 @@ const updateEvents = (scrapedEvents) => {
     })
 }
 
-//
+// Dashboard
 app.get('/dashboard', verifyUser, (req, res) => {
     EventModel.find().then(events => {
         //console.log(events);
@@ -135,6 +137,7 @@ app.get('/dashboard', verifyUser, (req, res) => {
     });
 })
 
+//All users
 app.get('/all', (req, res) => {
     //console.log("testing log")
     UserModel.find().then((result) => {
@@ -144,6 +147,7 @@ app.get('/all', (req, res) => {
     })
 })
 
+//All Comments
 app.get('/allcomments', (req, res) => {
     Comments.find().then((result) => {
         res.send(result);
@@ -152,6 +156,7 @@ app.get('/allcomments', (req, res) => {
     })
 })
 
+//Register
 app.post('/Register', (req, res) => {
     const { Name, Email, Password } = req.body;
     bcrypt.hash(Password, 10)
@@ -162,6 +167,36 @@ app.post('/Register', (req, res) => {
         }).catch(err => res.json(err))
 })
 
+//Login
+app.post('/login', (req, res) => {
+    const { Email, Password } = req.body;
+    UserModel.findOne({ Email: Email })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(Password, user.Password, (err, response) => {
+                    if (response) {
+                        const token = jwt.sign({ Email: user.Email },
+                            "jwt-secret-key", { expiresIn: '30m' })
+                        res.cookie('token', token)
+                        let checkacc = check(Email);
+                        return res.json({ Status: "Success" })
+                    } else {
+                        return res.json("The password is incorrect")
+                    }
+                })
+            } else {
+                return res.json("No record existed")
+            }
+        })
+})
+
+//Logout
+app.get('/logout', (req, res) => {
+    res.clearCookie('token')
+    return res.json({logout : true})
+})
+
+//Test
 app.post('/test', verifyUser, (req, res) => {
     const email = req.decoded.Email;
     //console.log(email)
@@ -178,6 +213,7 @@ function check(email) {
         })
 }
 
+//Complete Profile
 app.post('/complete', upload.single('ProfilePicture'), async (req, res) => {
     try {
         //getting email from token
@@ -215,28 +251,7 @@ app.post('/complete', upload.single('ProfilePicture'), async (req, res) => {
     }
 })
 
-app.post('/login', (req, res) => {
-    const { Email, Password } = req.body;
-    UserModel.findOne({ Email: Email })
-        .then(user => {
-            if (user) {
-                bcrypt.compare(Password, user.Password, (err, response) => {
-                    if (response) {
-                        const token = jwt.sign({ Email: user.Email },
-                            "jwt-secret-key", { expiresIn: '30m' })
-                        res.cookie('token', token)
-                        let checkacc = check(Email);
-                        return res.json({ Status: "Success" })
-                    } else {
-                        return res.json("The password is incorrect")
-                    }
-                })
-            } else {
-                return res.json("No record existed")
-            }
-        })
-})
-
+//Get comments based on event ID
 app.get('/comments/:eventId', async (req, res) => {
     const eventId = req.params.eventId;
     try {
@@ -254,62 +269,18 @@ app.get('/comments/:eventId', async (req, res) => {
     }
 });
 
-app.get('/logout', (req, res) => {
-    res.clearCookie('token')
-    return res.json({logout : true})
-})
-
+//Chatbot
 app.post("/chat", async(req, res) => {
     const {prompt} = req.body
     try{
     const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
     const deploymentId = "events-hub";
-    const result = await client.getChatCompletions(deploymentId, [
-    {
-      "role": "system",
-      "content": "You are the assistant for providing information about upcoming events. Users will inquire about details such as event titles, dates, locations, and summaries. Your goal is to create informative and contextually appropriate responses.\n\n1. User Interaction:\n    - Users will initiate conversations with questions related to events.\n    - The assistant should respond in a friendly and informative manner, addressing queries about event details.\n\n2. Event Information:\n    - The dataset provided contains details about various events, including titles, dates, locations, categories, and summaries.\n\n3. Context Handling:\n    - Maintain context within the conversation to ensure coherent and relevant responses.\n    - If the user asks follow-up questions, the assistant should refer to previous responses or provide additional details.\n\n4. Output Format:\n    - The assistant should output responses in a structured format, including relevant event details such as titles, dates, and summaries.\n\nExample Queries:\n- \"Tell me about the upcoming events.\"\n- \"When is the next event taking place?\"\n- \"Give me more details about the 'Special Concert' event.\"\n\nEvaluation Criteria:\n- Responses should be accurate, informative, and contextually relevant.\n- The assistant should handle various types of queries related to event details effectively.\n"
-    },
-    {
-      "role": "user",
-      "content": "Tell me about the Qatar Airways Grand Prix of Qatar Moto GP 2023."
-    },
-    {
-      "role": "assistant",
-      "content": "The Qatar Airways Grand Prix of Qatar Moto GP 2023 is a sports event that will take place from 17 Nov to 19 Nov at the Lusail International Circuit. It's a thrilling race where top riders compete under dazzling lights for victory. You can find more information about the event [here](https://visitqatar.com/intl-en/events-calendar/motogp-qatar-airways-grand-prix-2023)"
-    },
-    {
-      "role": "user",
-      "content": "What is the World Innovation Summit for Education (WISE) 2023 about?"
-    },
-    {
-      "role": "assistant",
-      "content": "The World Innovation Summit for Education (WISE) 2023 is a business summit focused on building the future of education. It aims to engage in discussions and explore solutions to pressing educational challenges. The summit will be held from 28 Nov to 29 Nov at the Qatar National Convention Center. You can find more information [here](https://www.wise-qatar.org)"
-    },
-    {
-      "role": "user",
-      "content": "Tell me more about the Katara Traditional Dhow Festival."
-    },
-    {
-      "role": "assistant",
-      "content": "The Katara Traditional Dhow Festival is a free event held from 28 Nov to 02 Dec at Katara Beach. It's the 13th edition of the festival, where traditional dhows will be showcased. You can find more information [here](https://www.katara.net)."
-    },
-    {
-      "role": "user",
-      "content": "What is the Made in Qatar exhibition about?"
-    },
-    {
-      "role": "assistant",
-      "content": "The Made in Qatar exhibition is a free event taking place from 29 Nov to 02 Dec at the Doha Exhibition & Convention Center (DECC). It is a platform that brings together local companies and factories to support and promote Qatari products. You can find more information [here](https://www.madeinqatar.com.qa/)."
-    },
-    // {
-    //   "role": "user",
-    //   "content": "What sport events are being held?",
-    // },
-    {
-        "role": "assistant",
-        "content": prompt,
-    }
-    ],
+    jsonFile = "./prompts.json"
+    const fileData = await readFile(jsonFile, 'utf8');
+    const prompts = JSON.parse(fileData);
+    prompts[prompts.length -1]['content'] = prompt;
+
+    const result = await client.getChatCompletions(deploymentId, prompts,
     {
       temperature: 1,
       max_tokens: 256,
